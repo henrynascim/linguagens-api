3f7859ae52e59b0b5975524a3725b9ef4f7c035e
Henrique Nascimento <114836223+henrynascim@users.noreply.github.com>
2023-04-04 21:39:41 -0300

    entrada postman

--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,13 @@ public class LinguagemController {
     public List<Linguagem> obterLinguagens() {
         List<Linguagem> linguagens = repository.findAll();
         return linguagens;
+
+    }
+
+    @PostMapping("/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        Linguagem linguagemSalva = repository.save(linguagem);
+        return linguagemSalva;
 
     }
 
